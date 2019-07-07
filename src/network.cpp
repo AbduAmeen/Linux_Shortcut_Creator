b8a8001cfe79f1024ebdf689c4b3319600569bbf
Abdu Ameen <abdu.ameen000@gmail.com>
2019-07-07 11:06:28 -0400

    contiuing

--- src/network.cpp
+++ src/network.cpp
@@ -1,15 +1,87 @@
 #include "network.h"
 
 namespace Network {
+
+Server::Server(QObject* parent) : QTcpServer(parent) {
+    listen(QHostAddress::Any);
+}
+
+void Server::incomingConnection(qintptr socketDescriptor) {
+    Connection* connection = new Connection(socketDescriptor, this);
+    emit NewConnection(connection);
+}
+
 Client::Client() {
+    QObject::connect(&server,SIGNAL(NewConnection(Connection*)),this, SLOT(NewConnection(Connection*)));
+}
+
+void Client::SendMessage(const QString& message) {
 
 }
 
-Server::Server() {
+void Client::NewConnection(Connection* connection) {
 
 }
 
-Connection::Connection() {
+void Client::ConnectionError(QAbstractSocket::SocketError error) {
 
 }
+
+void Client::Disconnected() {
+
 }
+
+void Client::Ready() {
+
+}
+
+Connection::Connection(QObject *parent) :
+    QTcpSocket(parent),
+    m_cborreader(this),
+    m_cborwriter(this)
+{
+    m_state = NotConnected;
+}
+
+Connection::Connection(qintptr socketdesc, QObject* parent) :
+    Connection(parent) {
+    setSocketDescriptor(socketdesc);
+    m_cborreader.setDevice(this);
+}
+
+bool Connection::SendMessage(const MessageContainer &message) {
+    if (message.text.isEmpty() && message.image.size() == 0) {return false;}
+
+    MessageContainer container = std::move(message);
+
+    for (QImage image: container.image) {
+        if (image.isNull()) {
+            container.image.erase(container.image.begin());
+        }
+    }
+
+    uint totalamountofitems = 0;
+
+    if (!container.text.isEmpty()) {totalamountofitems++;}
+    totalamountofitems += container.image.size();
+
+    m_cborwriter.startMap(totalamountofitems);
+    m_cborwriter.append(Text);
+    m_cborwriter.append(container.text.toLatin1());
+
+    for (QImage image: container.image) {
+        QByteArray array;
+        QBuffer buffer(&array);
+        image.save(&buffer);
+        m_cborwriter.append(Image);
+        m_cborwriter.append(array);
+    }
+
+    m_cborwriter.endMap();
+    return true;
+}
+
+Connection::~Connection() {
+
+}
+} //namespace Network
--- src/network.h
+++ src/network.h
@@ -1,24 +1,87 @@
 #ifndef NETWORK_H
 #define NETWORK_H
 #include <QObject>
+#include <QTcpServer>
+#include <QByteArrayList>
+#include <QTcpSocket>
+#include <QCborStreamReader>
+#include <QCborStreamReader>
+#include <QImage>
+#include <QThread>
+#include <QBuffer>
 
 namespace Network {
+class Connection;
+class Client;
+class Server;
 
-class Server {
-public:
-    Server();
+struct MessageContainer {
+    std::vector<QImage> image;
+    QString text;
 };
 
-class Client {
+class Server : public QTcpServer {
+    Q_OBJECT
+public:
+    Server(QObject* parent = nullptr);
+signals:
+    void NewConnection(Connection *connection);
+protected:
+    void incomingConnection(qintptr socketDescriptor) override;
+};
+
+class Client : public QObject {
+    Q_OBJECT
+    enum ConnectionStatus {Online, Offline};
 public:
     Client();
+    inline QString GetNickname() {return m_nickname;}
+    inline QString GetEmail() {return m_email;}
+signals:
+    //TODO: Set all of the user handles to be by email
+    void NewMessage(const QString &from ,const QString &message);
+    void NewParticipant(const QString &nick);
+    void ParticipantDisconnected(const QString &nick);
+public slots:
+    void SendMessage(const QString& message);
+private:
+    //TODO: make server a seperate thread
+    Server server;
+    QString m_nickname;
+    QString m_email;
+    QString m_password;
+
+private slots:
+    void NewConnection(Connection* connection);
+    void ConnectionError(QAbstractSocket::SocketError error);
+    void Disconnected();
+    void Ready();
 };
 
-class Connection {
+class Connection : public QTcpSocket {
+    Q_OBJECT
 public:
-    Connection();
+    enum DataType {Ping, Pong, Image, Text, Undefined};
+    enum State {Ready, NotConnected, Working};
+    Connection(QObject *parent = nullptr);
+    Connection(qintptr socketdesc, QObject* parent = nullptr);
+    ~Connection();
+    inline QString GetName() {return m_username;}
+    inline State GetState() {return m_state;}
+    bool SendMessage(const MessageContainer &message);
+
+signals:
+    void ReadyForUse();
+    void NewMessage(const QString& from, const MessageContainer& message);
+
+private:
+    QCborStreamReader m_cborreader;
+    QCborStreamWriter m_cborwriter;
+    State m_state;
+    QString m_username;
+
+private slots:
+
 };
-} // end Network
-
-
+} //namespace Network
 #endif // NETWORK_H
